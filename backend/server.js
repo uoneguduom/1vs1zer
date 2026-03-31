3b5a0925fa59ece58ade27e8dc6109a418021830
Lucasefrei123 <lucas.barthelemy2005@gmail.com>
2026-03-31 13:46:31 +0200

    server.js

---
+++ backend/server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const http = require("http");
+const {WebSocketServer} = require("ws");
+
+const app = express();
+const server= http.createServer(app);
+const wss = new WebSocketServer({server});
+
+const clients = new Map();
+
+wss.on("connection", (ws) =>{
+    const id = crypto.randomUUID();
+    clients.set(id,ws);
+    ws.send(JSON.stringify({type : "init", id}));
+
+    ws.on("message", (data)=>{
+        const state = JSON.parse(data);
+        for (const [cid, client] of clients) {
+            if (cid !==id && client.readyState ===1)
+                client.send(JSON.stringify(state));
+        }
+    });
+
+    ws.on("close", () => clients.delete(id));
+});
