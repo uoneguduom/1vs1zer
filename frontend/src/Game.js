e04e638f7fb5f3ded27a3a5e275cdddee9e4edd9
alexis-da <alexis.dacruz40@gmail.com>
2026-03-31 14:08:37 +0200

    map

--- frontend/src/Game.js
+++ frontend/src/Game.js
@@ -1,18 +1,20 @@
 import DevCamera from "./DevCamera";
 import Light from "./Light";
-import * as THREE from "three"
+import * as THREE from "three";
 import Test from "./Test";
+import Map from "./Map";
 
 export default class Game {
   constructor(scene, renderer) {
     this.scene = scene;
-    this.renderer = renderer
-    this.light = new Light(scene)
-    this.devCamera = new DevCamera(renderer)
-    this.test = new Test(scene)
+    this.renderer = renderer;
+    this.light = new Light(scene);
+    this.devCamera = new DevCamera(renderer);
+    // this.test = new Test(scene);
+    this.map = new Map(scene);
   }
 
   animate(delta, elapsed) {
-    this.test.animate(delta)
+    // this.test.animate(delta);
   }
 }
--- frontend/src/Map.js
+++ frontend/src/Map.js
@@ -1,5 +1,11 @@
 import * as THREE from "three";
 
+const wallGeo = new THREE.BoxGeometry(1, 2, 1);
+const wallMat = new THREE.MeshStandardMaterial({ color: 0x00ffaa });
+
+const HalfwallGeo = new THREE.BoxGeometry(1, 1, 1);
+const pathMat = new THREE.MeshStandardMaterial({ color: 0x444444 });
+
 export default class Map {
   constructor(scene) {
     this.scene = scene;
@@ -7,5 +13,54 @@ export default class Map {
     this.scene.add(this.map);
     this.createMap();
   }
-  map = [];
+  createMap() {
+    const map = [
+      [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+      [1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1],
+      [1, 0, 0, 0, 1, 0, 0, 1, 0, 0, 2, 0, 1, 0, 0, 1, 0, 0, 0, 0, 1],
+      [1, 0, 1, 0, 1, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 1],
+      [1, 0, 1, 0, 0, 2, 0, 0, 0, 1, 0, 0, 0, 0, 2, 0, 0, 0, 1, 0, 1],
+      [1, 0, 1, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 2, 0, 0, 1],
+      [1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1],
+      [1, 0, 1, 0, 0, 0, 2, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 1, 0, 0, 1],
+      [1, 0, 1, 0, 1, 0, 0, 0, 1, 1, 0, 0, 2, 0, 0, 0, 1, 0, 0, 0, 1],
+      [1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1],
+      [1, 0, 2, 0, 0, 1, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 2, 0, 0, 1],
+      [1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 1],
+      [1, 0, 0, 1, 0, 0, 1, 0, 1, 0, 2, 0, 0, 1, 0, 0, 1, 0, 0, 0, 1],
+      [1, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 2, 0, 1, 0, 1],
+      [1, 0, 1, 1, 0, 0, 0, 0, 2, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 0, 1],
+      [1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 1, 0, 1],
+      [1, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 2, 0, 0, 0, 0, 0, 0, 1, 0, 1],
+      [1, 0, 2, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 1],
+      [1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 2, 0, 1, 0, 1, 1, 0, 1],
+      [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+      [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    ];
+    const offset = (map.length - 1) / 2;
+
+    map.forEach((row, z) => {
+      row.forEach((cell, x) => {
+        const posX = x - offset;
+        const posZ = z - offset;
+
+        if (cell === 1) {
+          const wall = new THREE.Mesh(wallGeo, wallMat);
+          wall.position.set(posX, 1, posZ);
+          this.map.add(wall);
+        } else if (cell === 2) {
+          const halfWall = new THREE.Mesh(HalfwallGeo, wallMat);
+          halfWall.position.set(posX, 0.5, posZ);
+          this.map.add(halfWall);
+        } else {
+          const tile = new THREE.Mesh(
+            new THREE.BoxGeometry(1, 0.1, 1),
+            pathMat,
+          );
+          tile.position.set(posX, 0, posZ);
+          this.map.add(tile);
+        }
+      });
+    });
+  }
 }
